837a874b50ded8b5f2867c513319be27e31ccd14
Roland Wen <78676749+Roland-Wen@users.noreply.github.com>
2022-10-31 16:25:05 -0700

    Update add.test.js

--- test/add.test.js
+++ test/add.test.js
@@ -1,4 +1,13 @@
 const add = require("../src/main");
-test("Returns 2 for 1 + 1", () => {
+
+test("Testing 1 + 1 = 2", () => {
     expect(add(1, 1)).toBe(2);
+});
+
+test("Testing 123 + 456 = 579", () => {
+    expect(add(123, 456)).toBe(579);
+});
+
+test("Testing -5 + 3 = -2", () => {
+    expect(add(-5, 3)).toBe(-2);
 });
