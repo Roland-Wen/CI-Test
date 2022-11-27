779ebc45bfa7d8360f5afff3735c9da0bbb81268
Roalnd Wen <hwen@ucsd.edu>
2022-11-27 11:14:44 -0800

    A

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -5,5 +5,5 @@ const {join} = require('path');
  */
 module.exports = {
   // Changes the cache location for Puppeteer.
-  cacheDirectory: join(__dirname, 'node_modules'),
+  cacheDirectory: join(__dirname, 'node_modules', 'electron-to-chromium'),
 };
