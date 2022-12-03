5c8f080c64e0e7f8966e4eb872b9d7ad11a4df91
Roland Wen <78676749+Roland-Wen@users.noreply.github.com>
2022-12-03 15:36:24 -0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,19 @@
+/**
+ * Add stuff
+ * @param {int} a - input 1
+ * @param {int} b - input 2
+ * @return {int} a+b
+ */
 function add(a, b) {
   return a + b;
 }
 
+/**
+ * Multiply stuff
+ * @param {int} a - input 1
+ * @param {int} b - input 2
+ * @return {int} a*b
+ */
 function multiply(a, b){
   return a * b;
 }
